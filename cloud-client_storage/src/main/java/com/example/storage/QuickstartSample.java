5dfdfc2641ae559bec98c06dd9827bf84c214062
YoungChen <niaomingjian@outlook.com>
2018-03-16 11:50:40 +0800

    upload to GCS

--- cloud-client_storage/src/main/java/com/example/storage/QuickstartSample.java
+++ cloud-client_storage/src/main/java/com/example/storage/QuickstartSample.java
@@ -1,15 +1,15 @@
 
 package com.example.storage;
 
-import com.google.auth.oauth2.ServiceAccountCredentials;
 import com.google.cloud.storage.Blob;
 import com.google.cloud.storage.BlobId;
+import com.google.cloud.storage.BlobInfo;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.BucketInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 
-import java.io.FileInputStream;
+import java.io.UnsupportedEncodingException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -17,8 +17,9 @@ public class QuickstartSample {
   public static void main(String... args) throws Exception {
     createBucket();
     
-    // download();
-
+    //upload();
+    
+    //download();
   }
 
   private static void createBucket() throws Exception {
@@ -28,7 +29,7 @@ public class QuickstartSample {
 //        .getService();
     
     Storage storage = StorageOptions.getDefaultInstance().getService();
-    String bucketName = "my-new-bucket-abc-20180314";
+    String bucketName = "my-new-bucket-abc-20180314001";
     // Creates the new bucket
     Bucket bucket = storage.create(BucketInfo.of(bucketName));
 
@@ -49,4 +50,19 @@ public class QuickstartSample {
 
     System.out.printf("Download successfully%n");
   }
+  
+  private static void upload() throws UnsupportedEncodingException {
+    // Instantiates a client
+   
+   // Set GOOGLE_APPLICATION_CREDENTIALS environment variable in advance
+   Storage storage = StorageOptions.getDefaultInstance().getService();
+
+   String bucketName = "nmj-my-bucket-name";
+   String blobName = "my_blob_name.txt";
+   BlobId blobId = BlobId.of(bucketName, blobName);
+   BlobInfo blobInfo = BlobInfo.newBuilder(blobId).setContentType("text/plain").build();
+   Blob blob = storage.create(blobInfo, "Hello, World!".getBytes("UTF-8"));
+   
+   System.out.printf("Upload successfully%n");
+ }
 }
