67a72cc234807e1c670f824e11cda58504147989
YoungChen <niaomingjian@outlook.com>
2018-03-21 10:56:55 +0800

    java-for-loop

---
+++ java-for-loop/src/main/java/com/nmj/forloop/MainClass.java
@@ -0,0 +1,68 @@
+package com.nmj.forloop;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MainClass {
+
+  public static void main(String[] args) {
+    String[] arr = new String[] { "str1", "str2", "stop", "str4" };
+
+    System.out.println("----------------test1-------------");
+    // array - for - iterate over array
+    for (int i = 0; i < arr.length; i++) {
+
+    }
+
+    System.out.println("----------------test2-------------");
+    // array - foreach - iterate over array
+    for (String str : arr) {
+      System.out.println(str);
+      if ("stop".equals(str)) {
+        break;
+      }
+    }
+
+    List<String> list = Stream.of("str1", "str2", "stop", "str4").collect(Collectors.toList());
+
+    System.out.println("----------------test3-------------");
+    // list - for - iterate over collection
+    for (Iterator<String> iterator = list.iterator(); iterator.hasNext();) {
+      String str = (String) iterator.next();
+      System.out.println(str);
+      if ("stop".equals(str)) {
+        break;
+      }
+    }
+
+    System.out.println("----------------test4-------------");
+    // list - foreach - iterate over Iterable
+    for (String str : list) {
+      System.out.println(str);
+      if ("stop".equals(str)) {
+        break;
+      }
+    }
+
+    // @formatter:off
+    // output:
+    // ----------------test1-------------
+    // ----------------test2-------------
+    // str1
+    // str2
+    // stop
+    // ----------------test3-------------
+    // str1
+    // str2
+    // stop
+    // ----------------test4-------------
+    // str1
+    // str2
+    // stop
+    // @formatter:on
+
+  }
+
+}
